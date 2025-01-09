cbb2dd04b393281ca46177e57fcc371e7e2c2c25
Shell Ou <aa224689@gmail.com>
2025-01-08 22:42:46 -0800

    code is completely removed

--- src/financecalculator/contribution.py
+++ src/financecalculator/contribution.py
@@ -19,8 +19,3 @@ def calculate_contribution(principal, future_value, annual_rate, n_periods):
         The payment amount per period required to reach the specified future value 
         or pay off the loan.
     """
-    # annual_rate /= 100  # Convert to decimal
-    # rate_per_period = annual_rate / 12  # Monthly rate
-    # if rate_per_period == 0:  # Handle zero interest case
-    #     return (principal - future_value) / n_periods
-    # return (principal * rate_per_period) / (1 - (1 + rate_per_period) ** -n_periods)
